e7555dbd12187ed5dfe98b3b9d17381914c8ea72
Huu Tuong Tu <83907151+huutuongtu@users.noreply.github.com>
2024-04-25 09:35:17 +0700

    Update test_query_ontology.py
    add more query

--- tool/test_query_ontology.py
+++ tool/test_query_ontology.py
@@ -13,7 +13,7 @@ base_query = """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"""
 
-#query all data has type fantasy
+#query all data has type Fantasy
 query = """
            SELECT ?book ?title ?author
                 WHERE {
@@ -21,6 +21,26 @@ query = """
                         ?book :hasAuthor ?author .
                         ?book :hasTitle ?title .
                         }
+    """
+print(list(default_world.sparql(base_query + query)))
+
+#this will return only item type Book not contain item subclass of Book (not like reason).
+query = """
+           SELECT ?book
+                WHERE {
+                        ?book rdf:type :Book .
+                      }
+    """
+print(list(default_world.sparql(base_query + query)))
+
+#this will return nothing => chuatenhungchiecnhan doesn't have author and title !?!
+query = """
+           SELECT ?book ?title ?author
+                WHERE {
+                        ?book rdf:type :Book .
+                        ?book :hasAuthor ?author .
+                        ?book :hasTitle ?title .
+                      }
     """
 print(list(default_world.sparql(base_query + query)))
 
